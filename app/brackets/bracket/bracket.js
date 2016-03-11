9a064f9265a3a8d5b1996f2f942c75a93053128a
Benoit Denis <benoit.denis.ph@gmail.com>
2016-03-11 10:08:13 +0100

    assigning tables to bracket working

--- app/brackets/bracket/bracket.js
+++ app/brackets/bracket/bracket.js
@@ -4,17 +4,58 @@ angular.module('app').directive('trnmtBracket', ['bracketsSrv', function(bracket
         scope: {
             bracket: '='
         },
+        templateUrl: 'brackets/bracket/bracket.html',
+        controller: 'bracketCtrl',
         link: function postLink(scope, element) {
 
+            var tablePattern = /\(T(\d+)\)\s/;
 
             var saveFn = function(data){
-              scope.bracket.data = data;
+                scope.bracket.data = data;
                 bracketsSrv.save();
             };
 
-            $(element).bracket({
-               init : scope.bracket.data,
-                save : saveFn
+            var editFn = function(container, playerName, callback){
+                var tableNumber = null;
+                if(playerName.search(tablePattern) > -1){
+                    tableNumber = playerName.match(tablePattern)[1];
+                    playerName = playerName.replace(tablePattern, "");
+                }
+                scope.$apply(function(){
+                    scope.playerInfo.tableNumber = tableNumber;
+                    scope.playerInfo.name = playerName;
+                    scope.toggleModal();
+                    scope.lastCallback = callback;
+                });
+            };
+
+            scope.$watch("playerInfo.tableNumber", function(tableNumber){
+                if(scope.lastCallback == null){
+                    return;
+                }
+                var playerName = scope.playerInfo.name;
+                if(tableNumber > 0){
+                    var tableStr = "(T" + tableNumber + ") ";
+                    if(playerName.search(tablePattern) > -1){
+                        playerName = playerName.replace(tablePattern, tableStr);
+                    } else {
+                        playerName = tableStr + playerName;
+                    }
+                    scope.lastCallback(playerName);
+                } else {
+                    playerName = playerName.replace(tablePattern, "");
+                    scope.lastCallback(playerName);
+                }
+
+            });
+
+            $(element).find('.bracket').bracket({
+                init : scope.bracket.data,
+                save : saveFn,
+                decorator: {
+                    edit: editFn,
+                    render: function(container, name, score){container.append(name)}
+                }
             });
         }
     };
---
+++ app/brackets/bracket/bracketCtrl.js
@@ -0,0 +1,28 @@
+angular.module('app').controller('bracketCtrl', [ '$scope', 'tablesSrv', function($scope, tablesSrv){
+    tablesSrv.findAll();
+    $scope.tables = tablesSrv;
+    $scope.showModal = false;
+    $scope.toggleModal = function(){
+        $scope.showModal = !$scope.showModal;
+    };
+    $scope.playerInfo = {
+        name: null,
+        tableNumber: null,
+        lastCallback: null
+    };
+    $scope.freeTable = function(tableNumber){
+        tablesSrv.freeTable(tableNumber);
+        $scope.playerInfo.tableNumber = null;
+        tablesSrv.save();
+        $scope.toggleModal();
+    };
+    $scope.bookTable = function(tableNumber){
+        if($scope.playerInfo.tableNumber > 0){
+            tablesSrv.freeTable($scope.playerInfo.tableNumber);
+        }
+        tablesSrv.bookTable(tableNumber);
+        $scope.playerInfo.tableNumber = tableNumber;
+        tablesSrv.save();
+        $scope.toggleModal();
+    }
+}]);
