3a987ddc999374658582c1835429d245f84f3484
benoit.denis <benoit.denis.ph@gmail.com>
2019-01-03 22:30:27 +0100

    allow starting the application several time without sharing data if application is started from different folders

--- app/app.js
+++ app/app.js
@@ -2,6 +2,12 @@
 
 angular.module('app', ['ngRoute', 'LocalStorageModule']);
 
+angular.module('app').config(function (localStorageServiceProvider) {
+  localStorageServiceProvider
+    .setPrefix(window.location.href); // this is a trick to allow having the application multiple times in different folder
+    // and to not share the data between those folders
+});
+
 angular.module('app').value('isPositiveNumber', function(nbrStr){
     if(!nbrStr) return false;
     var nbr = parseInt(nbrStr);
